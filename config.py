60f4778dc7b87fca77cde41e0f8f6467fb4b9df6
eroxtic <125689731+eroxtic@users.noreply.github.com>
2023-02-18 01:26:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "29643784"))
+API_HASH = environ.get("API_HASH", "71767116ad831c1dcf288c3a72df3e7d")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6034515364:AAFJRN4KXqYzURQ8YpLPfBgxzGjF2aSOSQM")
+ADMIN = int(environ.get("ADMIN", "5072712936"))          
+CAPTION = environ.get("CAPTION", "@TriangleMoviesTG")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://te.legra.ph/file/cb38b76eba4ad1e4653e5.jpg")
